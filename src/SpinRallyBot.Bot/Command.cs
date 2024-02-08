47081f6ef0cdcf758497528a4d2649461cdc50f7
Alexander Salamatov <ch1seL@ya.ru>
2024-02-08 19:56:58 +0300

    implement RescheduleJob command

--- src/SpinRallyBot.Bot/Command.cs
+++ src/SpinRallyBot.Bot/Command.cs
@@ -18,14 +18,20 @@ public enum Command {
         Description = "Найти игрока по ФИО или ссылке на r.ttw.ru"
     )]
     [CommandArg("[Запрос]",
-        "Имя для поиска или ссылка на игрока(https://r.ttw.ru/players/?id=52a31ad)")]
+                "Имя для поиска или ссылка на игрока(https://r.ttw.ru/players/?id=52a31ad)")]
     Find,
 
     [Command(Text = "/update",
-        Description = "Обновление",
-        IsInitCommand = false,
-        IsAdminCommand = true)]
+             Description = "Обновление",
+             IsInitCommand = false,
+             IsAdminCommand = true)]
     Update,
+
+    [Command(Text = "/reschedule_job",
+             Description = "Обновление джобы",
+             IsInitCommand = false,
+             IsAdminCommand = true)]
+    RescheduleJob,
 
     [Command(
         Text = "/help",
---
+++ src/SpinRallyBot.Bot/Events/CommandReceivedConsumers/RescheduleJobConsumer.cs
@@ -0,0 +1,15 @@
+namespace SpinRallyBot.Events.CommandReceivedConsumers;
+
+public class RescheduleJobConsumer(ITelegramBotClient botClient, IScopedMediator mediator)
+    : CommandReceivedConsumerBase(Command.RescheduleJob, botClient, mediator) {
+    private readonly IScopedMediator _mediator = mediator;
+
+    protected override async Task ConsumeAndGetReply(long userId,
+                                                     long chatId,
+                                                     int? replyToMessageId,
+                                                     string[] args,
+                                                     bool isBotAdmin,
+                                                     CancellationToken cancellationToken) {
+        await _mediator.Send(new InitUpdaterJob(true), cancellationToken);
+    }
+}
